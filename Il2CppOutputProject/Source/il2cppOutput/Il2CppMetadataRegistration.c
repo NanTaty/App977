5ce6ad91c079082af87cf45345ada0e1ebfa9cb4
Assanali Amangeldiyev <deiteiai@gmail.com>
2024-09-27 19:03:04 +0500

    Initial commit

---
+++ Il2CppOutputProject/Source/il2cppOutput/Il2CppMetadataRegistration.c
@@ -0,0 +1,41 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern Il2CppGenericClass* const g_Il2CppGenericTypes[];
+extern const Il2CppGenericInst* const g_Il2CppGenericInstTable[];
+extern const Il2CppGenericMethodFunctionsDefinitions g_Il2CppGenericMethodFunctions[];
+extern const Il2CppType* const  g_Il2CppTypeTable[];
+extern const Il2CppMethodSpec g_Il2CppMethodSpecTable[];
+IL2CPP_EXTERN_C_CONST int32_t* g_FieldOffsetTable[];
+IL2CPP_EXTERN_C_CONST Il2CppTypeDefinitionSizes* g_Il2CppTypeDefinitionSizesTable[];
+IL2CPP_EXTERN_C const Il2CppMetadataRegistration g_MetadataRegistration;
+const Il2CppMetadataRegistration g_MetadataRegistration = 
+{
+	8410,
+	g_Il2CppGenericTypes,
+	5395,
+	g_Il2CppGenericInstTable,
+	43843,
+	g_Il2CppGenericMethodFunctions,
+	28055,
+	g_Il2CppTypeTable,
+	55836,
+	g_Il2CppMethodSpecTable,
+	8217,
+	g_FieldOffsetTable,
+	8217,
+	g_Il2CppTypeDefinitionSizesTable,
+	0,
+	NULL,
+};
